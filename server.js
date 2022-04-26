f61860a5ebe8a5abb797a551bfa61f0c6acc02cf
carterfm <carterf.morfitt@gmail.com>
2022-04-26 14:17:15 -0700

    Enabled cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const db = require('./config/connection');
 const routes = require('./controllers');
+// This is a necessary bit of middleware to allow our frontend to access this api
+const cors = require('cors');
 
 const PORT = 3001;
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 db.once('open', () => {
     app.listen(PORT, () => {
